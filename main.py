4847de114239358dc28e71d1cd39cb3b7d787e48
jan-salera <jann1n9@outlook.com>
2024-05-26 13:54:28 -0400

    added more to the AES graphs, trying to udpdate to wix

--- main.py
+++ main.py
@@ -33,20 +33,30 @@ def Key_Stats_KR():
     color_discrete_sequence=colors, width=180, height=300)
 
     st.plotly_chart(kr_fig)
-    
 
-def AES_Data():
+# Graphs for the AES 2023 Graduating Class Dataset
+def AES_Data_Major():
+    class_data = {
+    'Major': ["Applied Engineering Sciences", "Biosystems Engineering", "Computational Data Science", "Civil Engineering", 
+    "Chemical Engineering", "Computer Engineering", "Computer Science", "Electrical Engineering", "Enviornmental Engineering", 
+    "Mechanical Engineering", "Material Science"],
+    'Count': [78, 44, 14, 58, 95, 24, 227, 76, 17, 171, 16] }
+    colors = ['#CECECE', '#CECECE', '#CECECE','#18453B','#CECECE','#CECECE','#CECECE','#CECECE','#CECECE','#CECECE','#CECECE']
+    major_fig = px.pie(class_data, values='Count', names='Major', title='Major Distribution', color_discrete_sequence=colors)
+    st.plotly_chart(major_fig)
 
-    gender_data = {
-    'Gender': ['Male', 'Female', 'Other'],
-    'Count': [45, 35, 5] }
-    gender_fig = px.pie(gender_data, values='Count', names='Gender', title='Gender Distribution')
+def AES_Data_Gender():
+    gender_data = {'Gender': ['Male', 'Female'], 'Count': [51, 27]}
+    colors = ['#0B1799', '#C70F0F']
+    gender_fig = px.pie(gender_data, values='Count', names='Gender', title='Gender Distribution', color_discrete_sequence=colors)
     st.plotly_chart(gender_fig)
 
+def AES_Data_Ethnicity():
     ethnicity_data = {
-    'Ethnicity': ['White', 'Black', 'Hispanic', 'Asian', 'Other'],
-    'Count': [60, 20, 10, 5, 5]}
-    ethnicfig = px.pie(ethnicity_data, values='Count', names='Ethnicity', title='Ethnicity Distribution')
+    'Ethnicity': ['White', 'Asian', 'International', 'Hispanic/Latine', 'Black/African American', 'Two or More Races', 'Not Specified', 'American Indian/Alaskan Native'],
+    'Count': [58, 6, 6, 3, 2, 18, 2, 1]}
+    msu_colors = ['#18453B', '#008208', '#7BBD00', '#61BF67', '#49764C', '#0B9A6D', '#008934', "#C3C3C3"]
+    ethnicfig = px.pie(ethnicity_data, values='Count', names='Ethnicity', title='Ethnicity Distribution', color_discrete_sequence=msu_colors)
     st.plotly_chart(ethnicfig)
 
 def main():
@@ -58,9 +68,9 @@ def main():
     options = ("All Engineering Majors", "Applied Engineering Sciences", "Biosystems Engineering", "Computational Data Science", 
     "Civil Engineering", "Chemical Engineering", "Computer Engineering", "Computer Science", "Electrical Engineering", 
     "Enviornmental Engineering", "Mechanical Engineering", "Material Science")
-    ms = st.multiselect("empty label name", options=options, placeholder = "Filter By Major", label_visibility="collapsed")
+    ms = st.multiselect("Note: Make sure only one major is in the filter.", options=options, placeholder = "Filter By Major", label_visibility="visible")
 
-    if "All Engineering Majors" in ms:
+    if ["All Engineering Majors"] == ms:
         col1, col2, col3 = st.columns(3)
         with col1:
             st.title("")
@@ -85,7 +95,6 @@ def main():
             if kr_check:
                 Key_Stats_KR()
         
-        
         tab1, tab2, tab3 = st.tabs(["Major", "Ethnicity", "Gender"])
         with tab1:
             All_Data_Major()
@@ -94,38 +103,48 @@ def main():
         with tab3:
             All_Data_Gender()
 
-    elif "Applied Engineering Sciences" in ms:
-        st.write("You chose to sort by :green[Applied Engineering Sciences]")
-        AES_Data()
+    elif ["Applied Engineering Sciences"] == ms:
+        st.header("Spring 2023 Graduating Class Composition: AES Major")
+        tab1, tab2, tab3 = st.tabs(["Major", "Ethnicity", "Gender"])
+        with tab1:
+            AES_Data_Major()
+        with tab2:
+            AES_Data_Ethnicity()
+        with tab3:
+            AES_Data_Gender()
+        st.header("Co-Curricular Experiences")
+        
+        
+        
 
-    elif "Biosystems Engineering" in ms:
+    elif ["Biosystems Engineering"] == ms:
         st.write("You chose to sort by :green[Biosystems Engineering]")
 
-    elif "Computational Data Science" in ms:
+    elif ["Computational Data Science"] == ms:
         st.write("You chose to sort by :green[Computational Data Science]")
 
-    elif "Civil Engineering" in ms:
+    elif ["Civil Engineering"] == ms:
         st.write("You chose to sort by :green[Civil Engineering]")
 
-    elif "Chemical Engineering" in ms:
+    elif ["Chemical Engineering"] == ms:
         st.write("You chose to sort by :green[Chemical Engineering]")
 
-    elif "Computer Science" in ms:
+    elif ["Computer Science"] == ms:
         st.write("You chose to sort by :green[Computer Science]")
 
-    elif "Computer Engineering" in ms:
+    elif ["Computer Engineering"] == ms:
         st.write("You chose to sort by :green[Computer Engineering]")
         
-    elif "Electrical Engineering" in ms:
+    elif ["Electrical Engineering"] == ms:
         st.write("You chose to sort by :green[Electrical Engineering]")
 
-    elif "Enviornmental Engineering" in ms:
+    elif ["Enviornmental Engineering"] == ms:
         st.write("You chose to sort by :green[Enviornmental Engineering]")  
 
-    elif "Mechanical Engineering" in ms:
+    elif ["Mechanical Engineering"] == ms:
         st.write("You chose to sort by :green[Mechanical Engineering]") 
 
-    elif "Material Science" in ms:
+    elif ["Material Science"] == ms:
         st.write("You chose to sort by :green[Material Science]") 
     
 
