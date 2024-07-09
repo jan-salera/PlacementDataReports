427789c6bb60210497007f7b52021f7baeccf90a
shaynamcc <143293342+shaynamcc@users.noreply.github.com>
2024-07-08 17:19:24 -0700

    Update ShaynaChoroplethMapFunction.py

--- ShaynaChoroplethMapFunction.py
+++ ShaynaChoroplethMapFunction.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 
-def choropleth_state_map(file_path, name_of_major_category = "All"):
+def choropleth_state_map(file_path):
     all_majors_data = pd.read_csv(file_path)
     state_counts = all_majors_data['Employer State'].value_counts().reset_index()
     state_counts.columns = ['State', 'Count']
@@ -29,7 +29,6 @@ def choropleth_state_map(file_path, name_of_major_category = "All"):
         color_continuous_scale='Greens',
         labels={'LogCount': 'Log Count'},
         scope='usa',
-        title='Number of Employed ", name_of_major_category, " Spartan Graduates in the United States (Log-Scaled)',
         range_color=(state_counts['LogCount'].min(), state_counts['LogCount'].max()),  # Set the color scale range
         hover_data={'State': True, 'Count': True, 'StateAbbrev': False, 'LogCount': False}
     )
@@ -38,4 +37,16 @@ def choropleth_state_map(file_path, name_of_major_category = "All"):
 
 fig = choropleth_state_map("DestinationCumulativeDataset(Electrical Engineering).csv", "Electrical Engineering")
 st.subheader('Interactive Choropleth Map for States for Electrical Engineers')
+st.plotly_chart(fig)
+
+fig = choropleth_state_map("CSV_Spring 2021_2-21-24.csv")
+st.subheader('Interactive Choropleth Map for States for 2021 Spartan Engineering Graduates')
+st.plotly_chart(fig)
+
+fig = choropleth_state_map("CSV_Spring 2022_2-21-24.csv")
+st.subheader('Interactive Choropleth Map for States for 2022 Spartan Engineering Graduates')
+st.plotly_chart(fig)
+
+fig = choropleth_state_map("CSV_Spring 2023_3-7-24.csv")
+st.subheader('Interactive Choropleth Map for States for 2023 Spartan Engineering Graduates')
 st.plotly_chart(fig)
