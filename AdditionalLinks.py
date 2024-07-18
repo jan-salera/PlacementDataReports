71fb11c93c21fb2cf36c65b1ab0f5bed395d070d
jan-salera <jann1n9@outlook.com>
2024-07-18 18:13:15 -0400

    removed additional links

--- AdditionalLinks.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-J2, J1 = st.columns(2)
-with J2:
-    st.page_link("https://careernetwork.msu.edu/outcomes/", label= ":green[**Destination Data for All Majors**]")
-    st.image("CareerServices.png", "Contains MSU Wide - All Colleges Destination Survey (2023) by our Career Services Network")
-with J1:
-    st.page_link("https://engineering.msu.edu/academics/undergraduate-studies/enrollment-data", label= ":green[**College of Engineering Enrollment Data**]")
-    st.image("EnrollmentData.png", "Gives a year by year enrollment and graduation date breakdown for the last 10 years")
