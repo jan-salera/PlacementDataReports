2c29a0a312543af3b1a271362241ebee5409ca96
jan-salera <jann1n9@outlook.com>
2025-02-28 13:08:30 -0500

    added in applied engineering

--- DestinationData.py
+++ DestinationData.py
@@ -700,9 +700,13 @@ def main():
             with t3:
                 tab1, tab2= st.tabs(["By State", "By City"])
                 with tab1:
-                    st.header("INSERT MAPS HERE")
+                    fig = choropleth_state_map("CSV_Spring 2023_3-7-24.csv", "Applied Engineering Sciences")
+                    st.header('Interactive Map for Destination Data: Spring 2023 College of Engineering Graduating Class')
+                    st.plotly_chart(fig)
+                    top_5_employer_states("2023", "Applied Engineering Sciences")
                 with tab2:
-                    st.header("INSERT MAPS HERE")
+                    display_city_visualization("LATLONGDestinationCumulativeDataset(Applied Engineering Sciences).csv", "2023")
+                    display_top_5_cities("2023", "Applied Engineering Sciences")
         elif ["2022"] == ms1:
             t1, t2, t3 = st.tabs(["Key Statistics", "By Employer", "By Geography"])
             with t1:
@@ -724,9 +728,13 @@ def main():
             with t3:
                 tab1, tab2= st.tabs(["By State", "By City"])
                 with tab1:
-                    st.header("INSERT MAPS HERE")
+                    fig = choropleth_state_map("CSV_Spring 2022_2-21-24.csv", "Applied Engineering Sciences")
+                    st.header('Interactive Map for Destination Data: Spring 2022 College of Engineering Graduating Class')
+                    st.plotly_chart(fig)
+                    top_5_employer_states("2022", "Applied Engineering Sciences")
                 with tab2:
-                    st.header("INSERT MAPS HERE")
+                    display_city_visualization("LATLONGDestinationCumulativeDataset(Applied Engineering Sciences).csv", "2022")
+                    display_top_5_cities("2022", "Applied Engineering Sciences")
         elif ["2021"] == ms1:
             t1, t2, t3 = st.tabs(["Key Statistics", "By Employer", "By Geography"])
             with t1:
@@ -748,9 +756,13 @@ def main():
             with t3:
                 tab1, tab2= st.tabs(["By State", "By City"])
                 with tab1:
-                    st.header("INSERT MAPS HERE")
+                    fig = choropleth_state_map("CSV_Spring 2021_2-21-24.csv", "Applied Engineering Sciences")
+                    st.header('Interactive Map for Destination Data: Spring 2021 College of Engineering Graduating Class')
+                    st.plotly_chart(fig) 
+                    top_5_employer_states("2021", "Applied Engineering Sciences")
                 with tab2:
-                    st.header("INSERT MAPS HERE")
+                    display_city_visualization("LATLONGDestinationCumulativeDataset(Applied Engineering Sciences).csv", "2021")
+                    display_top_5_cities("2021", "Applied Engineering Sciences")
         elif ["Cumulative Data 21-23: Key Stats"] == ms1:
             t1, t2, t3 = st.tabs(["Key Statistics", "By Employer", "By Geography"])
             with t1:
@@ -760,13 +772,14 @@ def main():
             with t3:
                 tab1, tab2= st.tabs(["By State", "By City"])
                 with tab1:
-                    AESFig = choropleth_state_map("DestinationCumulativeDataset(Applied Engineering Sciences).csv")
+                    AllFig = choropleth_state_map("DestinationCumulativeDataset(All Majors).csv", "Applied Engineering Sciences")
                     st.header('College of Engineering Spring 21-23 Destination Locations - By State: AES Major')
-                    st.plotly_chart(AESFig) 
-                    top_5_employer_states("LATLONGDestinationCumulativeDataset(Applied Engineering Sciences).csv")
+                    st.plotly_chart(AllFig) 
+                    top_5_employer_states("Cumulative Data 21-23: Key Stats", "Applied Engineering Sciences")
                 with tab2:
                     st.header('College of Engineering Spring 21-23 Destination Locations - By City: AES Major')
-                    display_city_visualization("LATLONGDestinationCumulativeDataset(Applied Engineering Sciences).csv")
+                    display_city_visualization("LATLONGDestinationCumulativeDataset(Applied Engineering Sciences).csv", "Cumulative Data 21-23: Key Stats")
+                    display_top_5_cities("Cumulative Data 21-23: Key Stats", "Applied Engineering Sciences")
     
     elif ["Biosystems Engineering"] == ms:
         if ["2023"] == ms1:
